7656380e39e4dc18b832e641676f1f006a4af331
NightFury3000 <shubhsaxena447@gmail.com>
2021-06-15 13:46:35 +0530

    Updated the Customer Manager Script now it actually move customer from out of screen to where it should be then triggers dialogue and if he survives the paitent goes back to where he was. Also implemented Multiple Customer Functionality now Customer Manager Script will bring in next customer if we press start conversation after treating/killing first customer.

--- Assets/Scripts/CustomerManager.cs
+++ Assets/Scripts/CustomerManager.cs
@@ -5,12 +5,92 @@ using UnityEngine;
 public class CustomerManager : MonoBehaviour
 {
     public static CustomerManager cm;
-    public GameObject customer;
+
+    [SerializeField]
+    public GameObject[] customers;
+
+    [HideInInspector]
+    public GameObject activeCustomer;
+
+    private Vector2 WayPoint;
+    [SerializeField]
+    private Vector2 ExitTo;
+    [SerializeField]
+    private Vector2 EntryTo;
+    [SerializeField]
+    private float speed = 5.0f;
+
+    private int customerIndex = 0;
+
+    private bool destinationReached = true;
+
+    [SerializeField]
+    private GameObject NewCustomerButton;
     private void Awake()
     {
         if(cm == null)
         {
             cm = this;
         }
+    }
+
+    private void Update()
+    {
+        if (!destinationReached)
+        {
+            MoveToWayPoint();
+        }
+    }
+
+    private void MoveToWayPoint()
+    {
+        Vector2 currentLoc = activeCustomer.transform.position;
+        Vector2 distance =   WayPoint - currentLoc;
+
+        if (!(Mathf.Abs(distance.magnitude) < 2f))
+        {
+            activeCustomer.GetComponent<Rigidbody2D>().velocity = new Vector2(distance.normalized.x * speed,
+                                                                        distance.normalized.y * speed);
+        }
+        else
+        {
+            activeCustomer.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            destinationReached = true;
+            if (WayPoint.Equals(EntryTo))
+            {
+                activeCustomer.GetComponent<DialogueTrigger>().TriggerDialogue();
+            }
+            else
+            {
+                ReadyForNextCustomer(true);
+            }
+        }
+    }
+
+    public void EnterNewCustomer()
+    {
+        ReadyForNextCustomer(false);
+        if (customerIndex < customers.Length)
+        {
+            activeCustomer = customers[customerIndex++];
+            WayPoint = EntryTo;
+            destinationReached = false;
+        }
+        else
+        {
+            //Back To Main Menu or some credits/game over screen maybe?
+            Debug.Log("No More Customers!");
+        }
+    }
+
+    public void ExitCurrentCustomer()
+    {
+        WayPoint = ExitTo;
+        destinationReached = false;
+    }
+
+    public void ReadyForNextCustomer(bool ready)
+    {
+        NewCustomerButton.SetActive(ready);
     }
 }
